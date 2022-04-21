b0c29f667cd9f62b932c37485bbaa51268183b8c
Khusrav_Toshmatov <100082074+drrrcht@users.noreply.github.com>
2022-04-21 08:32:07 +0300

    Create 1009.cpp

---
+++ 1009.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+static long Calculate(int n, int k){
+  int * dp = new int [17];
+  dp[0] = 1;
+  dp[1] = k - 1;
+  for (int i = 2; i <= n; i++){
+    dp[i] = dp[i - 1] * (k - 1) + dp[i - 2] * (k - 1);
+  }
+  return dp[n];       
+}
+int main(){
+  int n,k;
+  std::cin>>n>>k;
+  std::cout<<Calculate(n, k);
+}
